c7285865ce480934f6f7b074df5b3ef72dae9e76
mail@philipgutjahr.de <gFL[5BSx]-Jc>
2022-06-08 08:01:51 +0200

    use canny for segmentation

--- colorCounter.py
+++ colorCounter.py
@@ -9,12 +9,14 @@ import numpy as np
 
 
 img = cv2.imread("images/voting-red_green-cards.jpg")
+
 blur = 1
 
 h_mean = 55
 h_range = 30
 s_ranges = (50, 255)
 l_ranges = (50, 255)
+
 
 # convert to HSV
 img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -26,7 +28,6 @@ img_hsv = cv2.GaussianBlur(img_hsv, (blur, blur), 0)
 # cv2.imshow('h', h)
 # cv2.imshow('s', s)
 # cv2.imshow('v', v)
-
 
 # create mask from hue range, including overflow
 # important: opencv divides hue values (360°) by 2 -> range 0-180
@@ -52,15 +53,26 @@ elif h_upper > 179:
 # clean mask
 d_shape = cv2.MORPH_ELLIPSE  # cv2.MORPH_RECT  cv2.MORPH_CROSS
 d_size = 1
-element = cv2.getStructuringElement(d_shape, (2 * d_size + 1, 2 * d_size + 1), (d_size, d_size))
-mask = cv2.dilate(mask, element)
+dilate_element = cv2.getStructuringElement(d_shape, (2 * d_size + 1, 2 * d_size + 1), (d_size, d_size))
+mask = cv2.dilate(mask, dilate_element)
 d_size = 2
-element = cv2.getStructuringElement(d_shape, (2 * d_size + 1, 2 * d_size + 1), (d_size, d_size))
-mask = cv2.erode(mask, element)
+erode_element = cv2.getStructuringElement(d_shape, (2 * d_size + 1, 2 * d_size + 1), (d_size, d_size))
+mask = cv2.erode(mask, erode_element)
 
+# find edges
+img_canny = cv2.Canny(img, 50, 200)
+img_canny = cv2.bitwise_not(img_canny)
+img_canny = cv2.erode(img_canny, dilate_element)
+img_canny = cv2.GaussianBlur(img_canny, (9, 9), 0)
+_, img_canny = cv2.threshold(img_canny, 145, 255, cv2.THRESH_BINARY)
 
-# find contours
+# combine masks
+mask = cv2.multiply(img_canny, mask)
+
+# find contours in combined mask
 cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+img_preview = img.copy()
 
 # loop over the contours
 MIN_THRESH = 200
@@ -74,17 +86,18 @@ for c in cnts[0]:
         cY = int(M["m01"] / M["m00"])
 
         # draw the contour and center of the shape on the image
-        cv2.drawContours(img, [c], -1, (0, 255, 0), 1)
-        cv2.putText(img, str(i), (cX-5, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+        cv2.drawContours(img_preview, [c], -1, (0, 255, 0), 1)
+        cv2.putText(img_preview, str(i), (cX-5, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
         i += 1
 
 
-# cv2.imshow("mask", mask)
+cv2.imshow("mask", mask)
 
 # # masked image
-# img_masked = cv2.bitwise_and(img, img, mask=mask)
+# img_masked = cv2.bitwise_and(img_preview, img_preview, mask=mask)
 # cv2.imshow("masked", img_masked)
 
-cv2.imshow("image", img)
+cv2.imshow("preview-image", img_preview)
+cv2.imshow("canny", img_canny)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
